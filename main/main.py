3fc442a52d6f133b0f8ed59d5fc2db8f893a738a
Ben McKenzie <bmckenzie0522@gmail.com>
2023-04-02 18:20:19 -0600

    first

---
+++ main/main.py
@@ -0,0 +1,8 @@
+import os
+
+char_name = "fartgit "
+char_age = "100.7"
+print("there once was a man named " + char_name + ",")
+print("he was " + char_age + " years old")
+print("he really liked the name " + char_name + ",")
+print("but did not like beiong " + char_age + ".")
